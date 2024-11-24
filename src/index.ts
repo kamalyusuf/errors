78a3d9c89c186c91cad688421e0580441b55fcc0
kamalyusuf <kbomoi@yahoo.com>
2024-11-24 22:03:36 +0100

    remove barrel files and tsup

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-export * from "./errors/index.js";
+export { BadRequestError } from "./errors/bad-request.js";
+export { ConflictError } from "./errors/conflict.js";
+export { CustomError } from "./errors/custom.js";
+export { ForbiddenError } from "./errors/forbidden.js";
+export { InternalServerError } from "./errors/internal-server.js";
+export { JoiValidationError } from "./errors/joi-validation.js";
+export { NotAuthorizedError } from "./errors/not-authorized.js";
+export { NotFoundError } from "./errors/not-found.js";
+export { RateLimitError } from "./errors/rate-limit.js";
+export { SomeError } from "./errors/some.js";
+export { UnprocessableEntityError } from "./errors/unprocessable-entity.js";
+export { ValidationError } from "./errors/validation.js";
 export { ERRORS, type ICustomError, setmessages as messages } from "./utils.js";
 export type { ErrorProps, ErrorStatus, ErrorSource } from "./types.js";
